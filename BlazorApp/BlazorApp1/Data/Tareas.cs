885f37fe65b4a7bdb5dffcb571361bb575f9d153
MicaelaLagana <lagana.mcl@gmail.com>
2020-11-17 01:47:06 -0300

    creando la base de la app

---
+++ BlazorApp/BlazorApp1/Data/Tareas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Data
+{
+    public class Tareas
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public DateTime Vencimiento { get; set; }
+        public int Estimacion { get; set; }
+        public Recurso Responsable { get; set; }
+        public bool Estado { get; set; }
+    }
+}
